bb4f74bb000b2663fbff1bd7186a48cbf2cd3230
Ashish Miglani <ashishmiglani02@gmail.com>
2017-06-05 21:50:57 +0530

    Final Assignment.java
    Finalised

--- Loktra/src/Assignment.java
+++ Loktra/src/Assignment.java
@@ -1,9 +1,9 @@
 
-class hashGame{
+class HashGame{
 	String letters;
 	
 	
-	hashGame(){
+	HashGame(){
 		letters="acdegilmnoprstuw";
 	}
 	
@@ -14,22 +14,37 @@ class hashGame{
 		
 		return reversedS;
 	}
-	String hashToString(long hashed){
+	String hashToString(String value){ //This function calculates and returns a string, if it exists
+		long hashed=0;
+		try{ //Checking if the string contains a valid long value
+			hashed=Long.parseLong(value);
+		}catch(Exception e){
+			System.out.println("Kindly check the value you entered and try again");
+			return null;
+		}
 		String newString="";
-		while(hashed>7){
+		while(hashed!= 7){
 			int i;
+			
+			//We will divide the index of each character and check if it is divisible by 37 or not
+			int flag=0;
 			
 			for( i=0;i<letters.length();i++){
 				if((hashed-i)%37==0){
 					newString+=letters.charAt(i);
 					hashed-=i;
 					hashed/=37;
+					flag =1;
 					break;
 					
 				}
 			}
+			if(flag==0){
+				System.out.println("No string found for this value");
+				return null;
+			}
 		}
-		if(newString!="")
+		if(newString.isEmpty()==false)
 			newString=reverseString(newString);
 		return newString;
 	}
@@ -39,7 +54,10 @@ class hashGame{
 
 public class Assignment{
 	public static void main(String[] args) {
-		long hashed=680131659347l;
-		System.out.println(""+new hashGame().hashToString(hashed));
+		HashGame hg= new HashGame();
+		String result=hg.hashToString(args[0]);
+		if(result!=null){
+			System.out.println("The String is "+result);
+		}
 	}
 }
